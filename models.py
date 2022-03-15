d1d7841340cff92ee85604024a4d5be51c077039
ggornes <gerardogornes@gmail.com>
2022-03-14 18:09:38 -0600

    (add) models

---
+++ models.py
@@ -0,0 +1,36 @@
+
+
+from sqlalchemy import Column, Integer, String, BIGINT, ForeignKey, func, FLOAT, DateTime
+from database import Base
+
+# Define To Do class inheriting from Base
+class Dispositivo(Base):
+    __tablename__ = 'dispositivos'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(256))
+    id_tipo_dispositivo = Column(BIGINT, ForeignKey("tipo_dispositivo.id"))
+    id_status_dispositivo = Column(BIGINT, ForeignKey("status_dispositivo.id"))
+    potencia_actual = Column(FLOAT(precision=10, decimal_return_scale=None))
+    fecha_alta = Column(DateTime(timezone=True), server_default=func.now())
+    fecha_actualizacion = Column(DateTime(timezone=True), onupdate=func.now())
+
+
+class TipoDispositivo(Base):
+    __tablename__ = "tipo_dispositivo"
+    id = Column(BIGINT, primary_key=True, index=True)
+    nombre_tipo_dispositivo = Column(String(255), unique=True)
+
+
+class StatusDispositivo(Base):
+    __tablename__ = "status_dispositivo"
+    id = Column(BIGINT, primary_key=True, index=True)
+    descripcion = Column(String(255), unique=True)
+
+
+class Lectura(Base):
+    __tablename__ = 'lecturas'
+    id = Column(Integer, primary_key=True)
+    id_dispositivo = Column(BIGINT, ForeignKey("dispositivos.id"))
+    id_tipo_dispositivo = Column(BIGINT, ForeignKey("tipo_dispositivo.id"))
+    potencia_actual: Column(FLOAT(precision=10, decimal_return_scale=None))
+    timestamp: Column(DateTime(timezone=True), server_default=func.now())
